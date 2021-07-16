3368e14a9b2164cdc0f243c4c121ec52a9c03dbf
thisisdylandev <dylan@ctrlaltdylan.com>
2021-07-15 23:14:22 -0500

    docs(app/models/workout.rb): remove commented out code

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,13 +3,4 @@ class Workout < ApplicationRecord
   validates_presence_of :sport
   belongs_to :sport
   has_many :workout_equipments
-
-  # def as_json(options={})
-  #   super(:except => [:created_at, :updated_at, :sport_id],
-  #     :include => {
-  #       :sport => { :only => [:name, :id] },
-  #       :workout_equipments
-  #     }
-  #   )
-  # end
 end
